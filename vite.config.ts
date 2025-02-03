19de12f7144244703ad069a0b8821b59504e40b3
Oleksandr Barchuk <b3181401@gmail.com>
2025-02-03 18:26:08 +0000

    add: possibility for csb preview

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ["w9wjpv-5173.csb.app", "localhost", "127.0.0.1"],
+  },
   plugins: [react()],
 });
